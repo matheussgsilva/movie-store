9a6f54dddcc7c4b3c6794e64cd50888c3a47cda3
Matheussgsilva <matheussgsilva@hotmail.com>
2022-05-30 19:47:22 -0300

    Finish the MovieCard

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -1,8 +1,27 @@
 import * as C from './styles'
+import { FaHeart, FaStar } from 'react-icons/fa'
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
     return (
-        <C.Container>Card</C.Container>
+        <C.Container>
+            <C.ContentArea>
+                <C.PosterArea>
+                    <img src={movie.poster} alt={movie.name} />
+                    <span>{movie.release}</span>
+                    <div><FaHeart /></div>
+                </C.PosterArea>
+                <C.InfoArea>
+                    <h3>{movie.name}</h3>
+                    <div>
+                        <span>{movie.rating}</span>
+                        <span><FaStar /></span>
+                        <span>{movie.genre}</span>
+                    </div>
+                    <p>{movie.price}</p>
+                </C.InfoArea>
+            </C.ContentArea>
+            <C.Button>Adicionar</C.Button>
+        </C.Container>
     )
 }
 
--- src/components/MovieCard/styles.js
+++ src/components/MovieCard/styles.js
@@ -1,5 +1,90 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 180px;
+    height: 320px;
+`;
 
+export const ContentArea = styled.div`
+    width: 100%;
+    border: 2px solid #C7D2DB;
+    border-bottom: none;
+    border-radius: 5px 5px 0px 0px;
+    box-sizing: border-box;
+    color: #4B5C6B;
+`;
+
+export const PosterArea = styled.div`
+    width: 100%;
+    height: 200px;
+    text-align: center;
+    border-bottom: 2px solid #C7D2DB;
+    position: relative;
+
+    img {
+        width: 100%;
+        height: 100%;
+        border-radius: 5px 5px 0px 0px;
+    }
+
+    span {
+        width: 100px;
+        position: absolute;
+        text-align: center;
+        margin-top: -150px;
+        background-color: #C7D2DB;
+        bottom: 0;
+        left: calc(50% - 50px);
+        border-radius: 5px 5px 0px 0px;
+        font-size: 0.8rem;
+    }
+
+    div {
+        position: absolute;
+        top: 0;
+        right: 0;
+        margin: 10px;
+        color: #FFF;
+        cursor: pointer;
+    }
+`;
+
+export const InfoArea = styled.div`
+    text-align: center;
+
+    h3 {
+        font-size: 1rem;
+        margin: 5px;
+    }
+
+    span {
+        font-size: 0.9rem;
+        margin-right: 5px;
+    }
+
+    p {
+        font-size: 1rem;
+        font-weight: bold;
+        margin-top: 3px;
+    }
+`;
+
+export const Button = styled.button`
+    border: none;
+    width: 100%;
+    padding: 8px;
+    background-color: #6558F5;
+    color: #FFF;
+    border-radius: 5px;
+    margin-top: -10px;
+    font-size: 0.9rem;
+    font-weight: bold;
+    cursor: pointer;
+
+    &:hover {
+        opacity: 0.8;
+    }
 `;
