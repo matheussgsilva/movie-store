b96b952b27afe5aa42f403b5d9306f2154202078
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-01 20:24:07 -0300

    Create the style of the MovieList

--- src/components/MovieList/styles.js
+++ src/components/MovieList/styles.js
@@ -1,5 +1,19 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    border-bottom: 0.5px solid #4B5C6B;
+    padding-bottom: 10px;
+`;
 
+export const Title = styled.h2`
+    color: #4B5C6B;
+    margin-left: 10px;
+`;
+
+export const List = styled.div`
+    display: flex;
+    column-gap: 20px;
+    padding: 0px 20px;
 `;
