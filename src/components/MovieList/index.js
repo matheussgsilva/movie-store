db749128b5faaa77f1adbad7a58fa46584871cb3
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-01 20:23:30 -0300

    Create the seesionTitle

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,12 +1,15 @@
 import * as C from './styles'
 import MovieCard from '../MovieCard'
 
-const MovieList = ({ movies }) => {
+const MovieList = ({ movies, sessionTitle }) => {
     return (
         <C.Container>
-            {movies.map(( movie ) => (
-                <MovieCard index={movie.id} movie={movie} />
-                ))}
+            <C.Title>{sessionTitle}</C.Title>
+            <C.List>
+                {movies.map(( movie ) => (
+                    <MovieCard index={movie.id} movie={movie} />
+                    ))}
+            </C.List>
         </C.Container>
     )
 }
