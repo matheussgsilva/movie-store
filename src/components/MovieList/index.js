4ffc104a7851f7607071d564ca1994f3a263961f
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-01 18:44:00 -0300

    Create MovieList component

---
+++ src/components/MovieList/index.js
@@ -0,0 +1,12 @@
+import * as C from './styles'
+
+
+const MovieList = ({ movies }) => {
+    return (
+        <C.Container>
+            Movie List
+        </C.Container>
+    )
+}
+
+export default MovieList
