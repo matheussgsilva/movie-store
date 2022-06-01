556c593f8cfefed758937c8d9a668b0cf6226c92
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-01 18:48:07 -0300

    Import MovieCard in MovieList and render them

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,10 +1,12 @@
 import * as C from './styles'
-
+import MovieCard from '../MovieCard'
 
 const MovieList = ({ movies }) => {
     return (
         <C.Container>
-            Movie List
+            {movies.map(( movie ) => (
+                <MovieCard index={movie.id} movie={movie} />
+                ))}
         </C.Container>
     )
 }
