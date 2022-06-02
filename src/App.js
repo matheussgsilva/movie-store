3b4a2716a2917f1cd08447049cd37d4bcb5fbf63
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-01 22:20:44 -0300

    Create movies state and useEffect to get api infos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Router from './router'
+import tmdb from './lib/tmdb'
 
 const App = () => {
   const [searchValue, setSearchValue] = useState('')
+  const [movies, setMovies] = useState([])
+
   console.log(searchValue)
+
+  useEffect(() => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${tmdb}&language=en-US&page=1`)
+    .then(res => res.json())
+    .then(data => setMovies(data.results))
+  }, [])
+
+  
 
   return (
     <>
@@ -13,7 +24,7 @@ const App = () => {
         onFavScreen={() => console.log('open Fav screen')}
         onCartScreen={() => console.log('open Cart screen')}
       />
-    <Router />
+    <Router movies={movies} />
     </>
   )
 }
