b00de1dbec735d2230c787dda2e0e81347a9a2e2
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-02 21:07:58 -0300

    Add movies state and fetch API

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,7 +1,17 @@
+import { useState, useEffect } from 'react'
 import * as C from './styles'
 import MovieCard from '../MovieCard'
+import tmdb from '../../lib/tmdb'
 
-const MovieList = ({ movies, sessionTitle }) => {
+const MovieList = ({ moviesId, sessionTitle }) => {
+    const [movies, setMovies] = useState([])
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${tmdb}&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=${moviesId}&with_watch_monetization_types=flatrate`)
+        .then(res => res.json())
+        .then(data => setMovies(data.results))
+      }, [])
+
     return (
         <C.Container>
             <C.Title>{sessionTitle}</C.Title>
