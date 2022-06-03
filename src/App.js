b01612654b4dc3ff4944de257a98edb5983a09cd
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-02 21:07:08 -0300

    Delete movies state and fetch

--- src/App.js
+++ src/App.js
@@ -1,30 +1,20 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Router from './router'
-import tmdb from './lib/tmdb'
+
 
 const App = () => {
   const [searchValue, setSearchValue] = useState('')
-  const [movies, setMovies] = useState([])
-
-  console.log(searchValue)
-
-  useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${tmdb}&language=en-US&page=1`)
-    .then(res => res.json())
-    .then(data => setMovies(data.results))
-  }, [])
-
-  
+  console.log(searchValue)  
 
   return (
     <>
     <Navbar 
         search={value => setSearchValue(value)}
         onFavScreen={() => console.log('open Fav screen')}
-        onCartScreen={() => console.log('open Cart screen')}
+        onCartScreen={() => console.log('open Cart screen')}       
       />
-    <Router movies={movies} />
+    <Router />
     </>
   )
 }
