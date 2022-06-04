77ec4c30dd336db2b9b3393c53ff7ad85d7dba52
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-04 14:16:37 -0300

    Import MovieDetail to router

--- src/router.js
+++ src/router.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import MovieDetail from './pages/MovieDetail'
 
 const Router = () => {
     return (
     <BrowserRouter>
       <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/detail/:id' element={<MovieDetail />} />
       </Routes>
     </BrowserRouter>
     )
