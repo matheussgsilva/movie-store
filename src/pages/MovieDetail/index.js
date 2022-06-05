1064101103d9c22c46ab07a5b29526d48c316cfe
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-05 19:41:02 -0300

    Change html tag to styled-components tags and add conditional to render release date and homepage link

--- src/pages/MovieDetail/index.js
+++ src/pages/MovieDetail/index.js
@@ -1,30 +1,40 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import * as C from './styles'
+import { FaHeart, FaStar } from 'react-icons/fa'
+import tmdb from '../../lib/tmdb'
 
 const MovieDetail = () => {
     const [movieDetail, setMovieDetail] = useState([])
     const { id } = useParams()
-    
+    const release = String(movieDetail.release_date)
 
     useEffect(() => {
-        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=5e0ac166209d17595d2a5c230da806c7&language=pt-BR`)
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${tmdb}&language=pt-BR`)
         .then(res => res.json())
-        .then(data => setMovieDetail(data) )
-    }, [])
-   
+        .then(data => setMovieDetail(data))
+    }, [])   
 
     return (
         <C.Container>
             <C.Detail>
                 <C.InfoArea>
-                    <h2>{movieDetail.title}</h2>
-                    <span>{movieDetail.vote_average}</span>
-                    <span>{movieDetail.release_date}</span>
-
-                    <a href={`${movieDetail.homepage}`} target="_blank" rel='noreferrer'>Acessar site oficial</a>
-
-                    <p>{movieDetail.overview}</p>
+                    <C.MovieTitle>{movieDetail.title}</C.MovieTitle>
+                    <C.MovieInfo>
+                        <span>
+                            {movieDetail.vote_average}
+                        </span>
+                        <i>
+                            <FaStar />
+                        </i>
+                        <span>{release.slice(0,4)}</span>
+                    </C.MovieInfo>
+                    {movieDetail.homepage &&
+                        <C.MovieLink href={`${movieDetail.homepage}`} target="_blank" rel='noreferrer'>
+                            Acessar site oficial
+                        </C.MovieLink>                    
+                    }
+                    <C.MovieOverview>{movieDetail.overview}</C.MovieOverview>
                 </C.InfoArea>
                     <img src={`https://image.tmdb.org/t/p/w500/${movieDetail.backdrop_path}`} alt={movieDetail.title} />
             </C.Detail>
