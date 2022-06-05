06a895b4837bfb88e6da38b363e275b9e5524405
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-04 21:06:25 -0300

    Create the MovieDetail structure with style

--- src/pages/MovieDetail/index.js
+++ src/pages/MovieDetail/index.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import * as C from './styles'
 
 const MovieDetail = () => {
-    const params = useParams()
+    const [movieDetail, setMovieDetail] = useState([])
+    const { id } = useParams()
     
+
+    useEffect(() => {
+        fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=5e0ac166209d17595d2a5c230da806c7&language=pt-BR`)
+        .then(res => res.json())
+        .then(data => setMovieDetail(data) )
+    }, [])
+   
+
     return (
         <C.Container>
-            Detalhe
+            <C.Detail>
+                <C.InfoArea>
+                    <h2>{movieDetail.title}</h2>
+                    <span>{movieDetail.vote_average}</span>
+                    <span>{movieDetail.release_date}</span>
+
+                    <a href={`${movieDetail.homepage}`} target="_blank" rel='noreferrer'>Acessar site oficial</a>
+
+                    <p>{movieDetail.overview}</p>
+                </C.InfoArea>
+                    <img src={`https://image.tmdb.org/t/p/w500/${movieDetail.backdrop_path}`} alt={movieDetail.title} />
+            </C.Detail>
         </C.Container>
     )
 }
--- src/pages/MovieDetail/styles.js
+++ src/pages/MovieDetail/styles.js
@@ -1,5 +1,31 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    display: flex;
+`;
 
+export const Detail = styled.div`
+    display: flex;
+
+    img {
+        width: 65vw;
+        margin-right: 0px;
+        margin-left: -250px;
+    }
+`;
+
+export const InfoArea = styled.div`
+    width: 50vw;
+    background: linear-gradient(45deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 70%, rgba(255,255,255,0) 100%);
+    z-index: 1;    
+    padding: 5vw;
+
+    h2 {
+        max-width: 300px;
+    }
+
+    p{
+        display: block;
+        width: 30vw;
+    }
 `;
