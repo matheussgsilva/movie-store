5fd725a4eda363e360539facca6c11b6937cc4ca
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-09 20:41:16 -0300

    Import FavoriteMovies

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import MovieDetail from './pages/MovieDetail'
+import FavoriteMovies from './pages/FavoriteMovies'
 
 const Router = () => {
     return (
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/detail/:id' element={<MovieDetail />} />
+          <Route path='/favoritos' element={<FavoriteMovies />} />
       </Routes>
     </BrowserRouter>
     )
