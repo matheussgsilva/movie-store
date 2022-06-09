58583c65af5171b60be74acc1444b9e7b7ddc419
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-09 20:40:40 -0300

    Create Favorite Movies component

---
+++ src/pages/FavoriteMovies/index.js
@@ -0,0 +1,11 @@
+import * as C from './styles'
+
+const FavoriteMovies = () => {
+    return (
+        <C.Container>
+            Lista de Favoritos
+        </C.Container>
+    )
+}
+
+export default FavoriteMovies
