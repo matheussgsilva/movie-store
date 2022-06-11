83c5eac73999df80779db387b1d79b67ccf34b3a
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-11 19:12:38 -0300

    Remove Navbar component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
-import { useState } from 'react'
-import Navbar from './components/Navbar'
 import Router from './router'
 
 
 const App = () => {
-  const [searchValue, setSearchValue] = useState('')
-  console.log(searchValue)  
-
   return (
-    <>
-    <Navbar 
-        search={value => setSearchValue(value)}
-        onCartScreen={() => console.log('open Cart screen')}       
-      />
-    <Router />
+    <>    
+      <Router />
     </>
   )
 }
