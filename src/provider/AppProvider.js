d0ae2746dc68e4f8812aae086b0df4b4ecdb19e3
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-12 19:39:37 -0300

    Change favoriteMovies to array of numbers

--- src/provider/AppProvider.js
+++ src/provider/AppProvider.js
@@ -3,18 +3,18 @@ import React, { useEffect, useState } from "react"
 export const AppContext = React.createContext({})
 
 export const AppProvider = (props) => {
-  const [favoriteMovies, setFavoriteMovies] = useState([{
-    id: '',
-  }]);
+  const [favoriteMovies, setFavoriteMovies] = useState([
+    '',
+  ]);
 
   useEffect(() => {
     const userStorage = localStorage.getItem("favoriteMovies")
     if (userStorage) {
       setFavoriteMovies(JSON.parse(userStorage))
     } else {
-        setFavoriteMovies([{
-        id: '',
-      }])
+        setFavoriteMovies([
+        '',
+      ])
     }
   }, [])
 
