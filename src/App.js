262ba96aeca4146588b440111be2c9903df77664
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-12 18:16:48 -0300

    Add AppProvider in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Router from './router'
+import { AppProvider } from './provider/AppProvider'
 
 
 const App = () => {
   return (
-    <>    
+    <AppProvider>    
       <Router />
-    </>
+    </AppProvider>
   )
 }
 
