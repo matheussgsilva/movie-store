3a799023953757720e36f9c2dcdbf014777e618c
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-12 18:52:27 -0300

    Import useApp and create function handleFavoriteMovie

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -1,8 +1,20 @@
 import { Link } from 'react-router-dom'
 import * as C from './styles'
 import { FaHeart, FaStar, FaInfoCircle } from 'react-icons/fa'
+import { useApp } from '../../provider/AppProvider'
 
-const MovieCard = ({ movie, favoriteMovie }) => {
+const MovieCard = ({ movie }) => {
+    const { favoriteMovies, setFavoriteMovies } = useApp()
+
+    const handleFavoriteMovie = () => {
+        let newFavorite = [...favoriteMovies];
+        newFavorite.push({
+            id: movie.id,
+        });
+        setFavoriteMovies(newFavorite);
+    }
+            
+
     return (
         <C.Container>
             <C.ContentArea>
@@ -11,7 +23,7 @@ const MovieCard = ({ movie, favoriteMovie }) => {
                         <img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
                     </Link>
                     <span>{movie.release_date}</span>
-                    <i className='favorite' onClick={() => favoriteMovie(movie.id)}><FaHeart /></i>
+                    <i className='favorite' onClick={handleFavoriteMovie}><FaHeart /></i>
                     <i className='info'><FaInfoCircle /></i>
                 </C.PosterArea>
                 <C.InfoArea>
