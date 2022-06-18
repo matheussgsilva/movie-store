00ce6e458a247ab1f4e4f510cfc96e49d970a902
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-18 19:24:25 -0300

    Create the CartItem structure with initial style

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -1,9 +1,28 @@
 import * as C from './styles'
+import { FaStar, FaTrash } from 'react-icons/fa'
 
-const CartItem = () => {
+const CartItem = ({ movie }) => {
     return (
         <C.Container>
-            Item
+            <C.PosterArea src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
+            <C.InfoArea>
+                <h3>{movie.title}</h3>
+                <p><strong>Título original: </strong>{movie.original_title}</p>
+                <div>
+                    <strong>Avaliação: </strong>
+                    <span>{movie.vote_average}</span>
+                    <i><FaStar /></i>
+                </div>
+                <p><strong>Ano de lançamento: </strong>{movie.release_date.slice(0,4)}</p>
+                <button><FaTrash /> Remover Filme</button>
+            </C.InfoArea>
+            <C.SelectArea>
+                <label>Selecione uma opção:</label>
+                <select>
+                    <option>Alugar</option>
+                    <option>Compar</option>
+                </select>
+            </C.SelectArea>
         </C.Container>
     )
 }
--- src/components/CartItem/styles.js
+++ src/components/CartItem/styles.js
@@ -1,5 +1,19 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+`;
+
+export const PosterArea = styled.img`
+
+`;
+
+export const InfoArea = styled.div`
+
+`;
+
+export const SelectArea = styled.div`
 
 `;
