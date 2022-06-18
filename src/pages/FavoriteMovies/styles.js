d6d961d518a112a91bc02e9a3d037078481a1c26
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-18 16:37:54 -0300

    Create the style page components

--- src/pages/FavoriteMovies/styles.js
+++ src/pages/FavoriteMovies/styles.js
@@ -1,5 +1,19 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 10px;
+`;
 
+export const Title = styled.h2`
+    color: #4B5C6B;
+    margin-left: 10px;
+`;
+
+export const List = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    column-gap: 20px;
+    padding: 0px 20px;
 `;
