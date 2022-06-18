c4b9269d3f397d312789fc225e3f1fb5e7589bad
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-18 16:31:45 -0300

    Change the initial format of the favorite movies

--- src/provider/AppProvider.js
+++ src/provider/AppProvider.js
@@ -3,18 +3,14 @@ import React, { useEffect, useState } from "react"
 export const AppContext = React.createContext({})
 
 export const AppProvider = (props) => {
-  const [favoriteMovies, setFavoriteMovies] = useState([
-    '',
-  ]);
+  const [favoriteMovies, setFavoriteMovies] = useState([]);
 
   useEffect(() => {
     const userStorage = localStorage.getItem("favoriteMovies")
     if (userStorage) {
       setFavoriteMovies(JSON.parse(userStorage))
     } else {
-        setFavoriteMovies([
-        '',
-      ])
+        setFavoriteMovies([])
     }
   }, [])
 
