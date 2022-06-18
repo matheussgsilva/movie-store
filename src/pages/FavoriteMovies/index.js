e4cae1ca6189c6e72df96905cbd9c26692824773
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-18 16:35:26 -0300

    Remove hooks import and add MovieCard

--- src/pages/FavoriteMovies/index.js
+++ src/pages/FavoriteMovies/index.js
@@ -1,16 +1,24 @@
 import * as C from './styles'
 import Theme from '../../components/Theme'
 import { useApp } from '../../provider/AppProvider'
+import MovieCard from '../../components/MovieCard'
 
 const FavoriteMovies = () => {
-    const { favoriteMovies } = useApp()
-
-    console.log(favoriteMovies)
+    const { favoriteMovies } = useApp()   
 
     return (
         <Theme>
             <C.Container>
-                Lista de Favoritos
+            <C.Title>Lista de Favoritos</C.Title>
+            <C.List>
+                {favoriteMovies.map(( movie ) => (
+                    <MovieCard 
+                        key={movie.id} 
+                        movie={movie}
+                        favorite={true}
+                    />
+                ))}
+            </C.List>
             </C.Container>
         </Theme>
     )
