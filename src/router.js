c9ce72ccc1fa181b6e2941f94ac4caa5dd37d6ee
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-18 19:21:42 -0300

    Add Cart page to router

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import MovieDetail from './pages/MovieDetail'
 import FavoriteMovies from './pages/FavoriteMovies'
+import Cart from './pages/Cart'
 
 const Router = () => {
     return (
@@ -10,6 +11,7 @@ const Router = () => {
           <Route path='/' element={<Home />} />
           <Route path='/detail/:id' element={<MovieDetail />} />
           <Route path='/favoritos' element={<FavoriteMovies />} />
+          <Route path='/carrinho' element={<Cart />} />
       </Routes>
     </BrowserRouter>
     )
