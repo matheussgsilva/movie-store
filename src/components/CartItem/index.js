a8ecc3c5953027677cce855192f138f463c233b5
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-28 20:28:48 -0300

    Refactor the tags and create MovieValue

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -6,15 +6,15 @@ const CartItem = ({ movie }) => {
         <C.Container>
             <C.PosterArea src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} alt={movie.title} />
             <C.InfoArea>
-                <h3>{movie.title}</h3>
-                <p><strong>Título original: </strong>{movie.original_title}</p>
-                <div>
+                <C.MovieTitle>{movie.title}</C.MovieTitle>
+                <C.OriginalMovieTitle><strong>Título original: </strong>{movie.original_title}</C.OriginalMovieTitle>
+                <C.RateArea>
                     <strong>Avaliação: </strong>
                     <span>{movie.vote_average}</span>
                     <i><FaStar /></i>
-                </div>
-                <p><strong>Ano de lançamento: </strong>{movie.release_date.slice(0,4)}</p>
-                <button><FaTrash /> Remover Filme</button>
+                </C.RateArea>
+                <C.ReleaseDate><strong>Ano de lançamento: </strong>{movie.release_date.slice(0,4)}</C.ReleaseDate>
+                <C.Remove><FaTrash /> Remover Filme</C.Remove>
             </C.InfoArea>
             <C.SelectArea>
                 <label>Selecione uma opção:</label>
@@ -22,6 +22,7 @@ const CartItem = ({ movie }) => {
                     <option>Alugar</option>
                     <option>Compar</option>
                 </select>
+                <C.MovieValue>Valor: <br/><strong>R$ 13,50</strong></C.MovieValue>
             </C.SelectArea>
         </C.Container>
     )
