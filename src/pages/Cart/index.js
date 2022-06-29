9dfa1b80888e30dc3a22c53be9314c9c9673e16d
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-29 20:17:06 -0300

    Create CartListHeader and Resume area with style

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,7 @@
 import * as C from './styles'
 import Theme from '../../components/Theme'
 import CartItem from '../../components/CartItem'
+import { FaLock } from 'react-icons/fa'
 
 const Cart = () => {
     const movie = {"adult":false,"backdrop_path":"/zGLHX92Gk96O1DJvLil7ObJTbaL.jpg","genre_ids":[14,12,28],"id":338953,"original_language":"en","original_title":"Fantastic Beasts: The Secrets of Dumbledore","overview":"Professor Albus Dumbledore knows the powerful, dark wizard Gellert Grindelwald is moving to seize control of the wizarding world. Unable to stop him alone, he entrusts magizoologist Newt Scamander to lead an intrepid team of wizards and witches. They soon encounter an array of old and new beasts as they clash with Grindelwald's growing legion of followers.","popularity":3670.155,"poster_path":"/jrgifaYeUtTnaH7NF5Drkgjg2MB.jpg","release_date":"2022-04-06","title":"Fantastic Beasts: The Secrets of Dumbledore","video":false,"vote_average":6.9,"vote_count":1824}
@@ -9,6 +10,18 @@ const Cart = () => {
         <Theme>
             <C.Container>
                 <C.CartList>
+                    <C.CartListHeader>
+                        <C.CartListHeaderInfo>
+                            <p>Sacola de compras - Movie Store</p>
+                            <span> 3 itens</span>
+                        </C.CartListHeaderInfo>
+                        <C.CartListSecurity>
+                            <i>
+                                <FaLock />
+                            </i>
+                            <p>100% <br/><span>SEGURO</span></p>
+                        </C.CartListSecurity>
+                    </C.CartListHeader>
                     <CartItem 
                         key={movie.id}
                         movie={movie}
@@ -22,6 +35,28 @@ const Cart = () => {
                         movie={movie}
                     />
                 </C.CartList>
+                <C.Resume>
+                    <C.ResumeHeader>
+                        <span>RESUMO</span>
+                    </C.ResumeHeader>
+                    <C.ResumeSubtotal>
+                        <span>Subtotal (3 itens)</span>
+                        <strong>R$123,00</strong>
+                    </C.ResumeSubtotal>
+                    <C.ResumeTotal>
+                        <span>Total</span>
+                        <C.TotalValue>
+                            <p><strong>R$123,00</strong></p>
+                            <p>em até 6x de <span>RS21,00</span></p>
+                        </C.TotalValue>
+                    </C.ResumeTotal>
+                    <C.FinishButton>
+                        finalizar compra
+                    </C.FinishButton>
+                    <C.ShoppingButton>
+                        continuar comprando
+                    </C.ShoppingButton>
+                </C.Resume>
             </C.Container>
         </Theme>
     )
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -3,8 +3,147 @@ import styled from 'styled-components'
 export const Container = styled.div`
     display: flex;
     justify-content: center;
+    position: relative;
 `;
 
 export const CartList = styled.div`
+    padding-top: 20px;
+`;
 
+export const CartListHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #DDD;
+    max-width: 800px;
+    padding: 10px;
+    box-sizing: border-box;
+`;
+
+export const CartListHeaderInfo = styled.div`
+    text-transform: uppercase;
+
+    p {
+        font-size: 0.8rem;
+        font-weight: bold;
+        color: #333333;
+        margin: 0;
+        
+    }
+
+    span {
+        font-size: 0.8rem;
+        color: #333333;
+    }
+`;
+
+export const CartListSecurity = styled.div`
+    display: flex;
+    align-items: center;
+    color: #999999;
+
+    i {
+        font-size: 1.5rem;        
+        margin-right: 5px;
+    }
+
+    p {
+        font-size: 0.9rem;
+        margin: 0;
+        line-height: 12px;
+        text-align: center;
+    }
+
+    span {
+        font-size: 0.7rem;
+    }
+`;
+
+export const Resume = styled.div`
+    padding-top: 20px;
+    margin-left: 10px;
+    position: sticky;
+`;
+
+export const ResumeHeader = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    background-color: #DDD;
+    width: 300px;
+    padding: 20px 10px;
+    box-sizing: border-box;    
+
+    span {
+        font-size: 0.9rem;
+        font-weight: bold;
+        color: #333333;
+        margin: 0;
+        text-transform: uppercase;
+    }
+
+`;
+
+export const ResumeSubtotal = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 30px 20px;
+    font-size: 0.9rem;
+    color: #333333;
+    border-bottom: 1px solid #999999;
+`;
+
+export const ResumeTotal = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;    
+    background-color: #DDD;
+    padding: 20px;
+    color: #333333;
+
+    span {
+        font-weight: bold;
+    }
+`;
+
+export const TotalValue = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end;
+    justify-content: center;
+
+    p {
+        font-size: 0.8rem;
+        margin: 0;
+    }
+
+    strong {
+        font-size: 1rem;
+    }
+`;
+
+export const FinishButton = styled.button`
+    display: block;
+    margin-top: 10px;
+    width: 100%;
+    padding: 15px;
+    text-transform: uppercase;
+    font-size: 0.9rem;
+    font-weight: bold;
+    background-color: #8DD7CF;
+    color: #FFF;
+    border: none;
+    cursor: pointer;
+    transition: 300ms ease-in-out;
+
+    &:hover {
+        opacity: 0.8;
+    }
+`;
+
+export const ShoppingButton = styled(FinishButton)`
+    border: 2px solid #8DD7CF;
+    background-color: #FFF;
+    color: #8DD7CF
 `;
