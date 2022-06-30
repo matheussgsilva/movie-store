81e9d02d7b3ef43d835d40120a0db2f4bcb37557
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-29 21:56:08 -0300

    Import useCart

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,10 +1,11 @@
 import * as C from './styles'
 import Theme from '../../components/Theme'
+import { useCart } from '../../provider/CartProvider'
 import CartItem from '../../components/CartItem'
 import { FaLock } from 'react-icons/fa'
 
 const Cart = () => {
-    const movie = {"adult":false,"backdrop_path":"/zGLHX92Gk96O1DJvLil7ObJTbaL.jpg","genre_ids":[14,12,28],"id":338953,"original_language":"en","original_title":"Fantastic Beasts: The Secrets of Dumbledore","overview":"Professor Albus Dumbledore knows the powerful, dark wizard Gellert Grindelwald is moving to seize control of the wizarding world. Unable to stop him alone, he entrusts magizoologist Newt Scamander to lead an intrepid team of wizards and witches. They soon encounter an array of old and new beasts as they clash with Grindelwald's growing legion of followers.","popularity":3670.155,"poster_path":"/jrgifaYeUtTnaH7NF5Drkgjg2MB.jpg","release_date":"2022-04-06","title":"Fantastic Beasts: The Secrets of Dumbledore","video":false,"vote_average":6.9,"vote_count":1824}
+    const { cart } = useCart()
 
     return (
         <Theme>
@@ -22,18 +23,12 @@ const Cart = () => {
                             <p>100% <br/><span>SEGURO</span></p>
                         </C.CartListSecurity>
                     </C.CartListHeader>
-                    <CartItem 
-                        key={movie.id}
-                        movie={movie}
-                    />
-                    <CartItem 
-                        key={movie.id}
-                        movie={movie}
-                    />
-                    <CartItem 
-                        key={movie.id}
-                        movie={movie}
-                    />
+                        {cart.map(( movie ) => (
+                            <CartItem 
+                                key={movie.id} 
+                                movie={movie}
+                            />
+                        ))}
                 </C.CartList>
                 <C.Resume>
                     <C.ResumeHeader>
