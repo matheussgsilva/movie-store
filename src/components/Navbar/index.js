e9d71598a910fce0b36d8225f697fbac78f88087
Matheussgsilva <matheussgsilva@hotmail.com>
2022-06-29 22:55:32 -0300

    Create favorite function and add Link to logo and cartIcon

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,17 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import * as C from './styles'
 import { FaShoppingCart } from 'react-icons/fa'
 import { FaHeart } from 'react-icons/fa'
 import { FaSearch } from 'react-icons/fa'
+import { useApp } from '../../provider/AppProvider'
 
-const Navbar = ({ search, onCartScreen }) => {
+const Navbar = ({ search }) => {
     const [searchField, setSearchField] = useState('')
+    const { favoriteMovies } = useApp()
+    const [favorite, setFavorite] = useState(false)
+
+    useEffect(() => {
+        if ( favoriteMovies.length === 0) {
+            setFavorite(false)
+        }else {
+            setFavorite(true)
+        }
+    }, [favoriteMovies])
 
     return (
         <C.Container>
             <C.LogoArea>
-                <h2>LOGO</h2>
+                <Link to={'/'}>
+                    <h2>LOGO</h2>
+                </Link>
             </C.LogoArea>
             <C.InputArea>
                 <input 
@@ -27,18 +40,15 @@ const Navbar = ({ search, onCartScreen }) => {
             </C.InputArea>
             <C.IconArea>                
                 <Link to={'/favoritos'}>
-                    <p>
+                    <C.HeartIcon movies={favorite}>
                         <FaHeart />
-                    </p>
+                    </C.HeartIcon>
                 </Link>
-                
-                <p>
-                    <span>
-                        <FaShoppingCart 
-                            onClick={onCartScreen}
-                        />
-                    </span>
-                </p>
+                <Link to={'/carrinho'}>
+                    <C.CartIcon>
+                        <FaShoppingCart />
+                    </C.CartIcon>
+                </Link>
             </C.IconArea>
         </C.Container>
     )
