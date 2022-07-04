ebc42a8dc34706ce302a35a25c8af85c29c4056a
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-04 20:37:09 -0300

    Add CartSidebar component, create showSidebar state and create Sidebar area with style

--- src/components/Theme/index.js
+++ src/components/Theme/index.js
@@ -1,17 +1,25 @@
 import { useState } from 'react'
-import Navbar from '../Navbar'
 import * as C from './styles'
+import Navbar from '../Navbar'
+import CartSidebar from '../CartSidebar'
 
 const Theme = ({ children }) => {
     const [searchValue, setSearchValue] = useState('')
-    console.log(searchValue)  
+    const [showSidebar, setShowSidebar] = useState(true)
+    
+    const handleShowSidebar = () => {
+        setShowSidebar(!showSidebar)
+    }
 
     return (
         <C.Container>
             <Navbar 
                 search={value => setSearchValue(value)}
-                onCartScreen={() => console.log('open Cart screen')}       
+                showCartSidebar={handleShowSidebar}       
             />
+            <C.Sidebar showSidebar={showSidebar} >
+                <CartSidebar />
+            </C.Sidebar>
             <C.Page>
                 {children}
             </C.Page>
--- src/components/Theme/styles.js
+++ src/components/Theme/styles.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+    position: relative;
+`;
 
+export const Sidebar = styled.aside`
+    position: fixed;
+    z-index: 1;
+    right: -450px;
+    top: 15vh;
+    transform: ${props => props.showSidebar ? 'translateX(-450px)' : 'translateX(10px)'};
+    transition: transform 300ms ease-in-out;
 `;
 
 export const Page = styled.div`
