9189ba78d3d680d161882069edc4f7ba48f4fd43
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-05 20:17:35 -0300

    Create new components with style and pass props from click

--- src/components/CartSidebar/index.js
+++ src/components/CartSidebar/index.js
@@ -1,13 +1,18 @@
 import * as C from './styles'
 import { Link } from 'react-router-dom'
 
-const CartSidebar = () => {
+const CartSidebar = ({ onHidden }) => {
     return (
         <C.Container>
-            Teste
+            <C.Title>Itens no carrinho</C.Title>
+            <C.ButtonArea>
             <Link to={'/carrinho'}>
-                <button>Cart</button>
+                <C.Cart>Ir para o carrinho</C.Cart>
             </Link>
+            <C.ShoppingButton onClick={onHidden}>
+                Continuar comprando
+            </C.ShoppingButton>
+            </C.ButtonArea>
         </C.Container>
     )
 }
--- src/components/CartSidebar/styles.js
+++ src/components/CartSidebar/styles.js
@@ -2,11 +2,33 @@ import styled from 'styled-components'
 
 export const Container = styled.div`
     display: flex;
-    flex-direction: column;
-    justify-content: center;
+    flex-direction: column;    
     align-items: center;
     width: 450px;
     height: 85vh;
-    background-color: gray;
+    background-color: #FFF;
     box-shadow: -5px 1px 6px -1px rgba(0,0,0,0.55);
+    position: relative;
+`;
+
+export const Title = styled.h3`
+    color: #333333;
+    text-align: center;
+`;
+
+export const ButtonArea = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    height: 120px;
+`;
+
+export const Cart = styled.span`
+    text-decoration: none;
+`;
+
+export const ShoppingButton = styled.button`
+
 `;
