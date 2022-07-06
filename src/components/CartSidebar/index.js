dc57c968d09c1e1834fa07bed180a80148521e29
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-06 20:00:34 -0300

    Import useCart and SidebarCartItem

--- src/components/CartSidebar/index.js
+++ src/components/CartSidebar/index.js
@@ -1,10 +1,18 @@
 import * as C from './styles'
 import { Link } from 'react-router-dom'
+import { useCart } from '../../provider/CartProvider'
+import SidebarCartItem from '../SidebarCartItem'
 
 const CartSidebar = ({ onHidden }) => {
+    const { cart, setCart } = useCart()
     return (
         <C.Container>
-            <C.Title>Itens no carrinho</C.Title>
+            <C.CartItemsArea>
+            <C.Title>Itens no carrinho ( {cart.length === 0 ? 'vazio' : cart.length} )</C.Title>
+            {cart.map(( movie ) => (
+                <SidebarCartItem movie={movie} />
+            ))}
+            </C.CartItemsArea>
             <C.ButtonArea>
             <Link to={'/carrinho'}>
                 <C.Cart>Ir para o carrinho</C.Cart>
