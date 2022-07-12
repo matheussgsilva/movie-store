ff8fdbdf88d1c7567049445274b4389a3fa89f5f
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-12 20:30:57 -0300

    Add SearchPage in Router

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import MovieDetail from './pages/MovieDetail'
 import FavoriteMovies from './pages/FavoriteMovies'
 import Cart from './pages/Cart'
+import SearchPage from './pages/SearchPage'
 
 const Router = () => {
     return (
@@ -12,6 +13,7 @@ const Router = () => {
           <Route path='/detail/:id' element={<MovieDetail />} />
           <Route path='/favoritos' element={<FavoriteMovies />} />
           <Route path='/carrinho' element={<Cart />} />
+          <Route path='/search/:id' element={<SearchPage />} />
       </Routes>
     </BrowserRouter>
     )
