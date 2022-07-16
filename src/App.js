2d4add6b1ffb3e4ba94aca16729918923077779e
Matheussgsilva <matheussgsilva@hotmail.com>
2022-07-16 20:04:48 -0300

    Import PriceProvider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Router from './router'
 import { AppProvider } from './provider/AppProvider'
 import { CartProvider } from './provider/CartProvider'
-
+import { PriceProvider } from './provider/PriceProvider'
 
 const App = () => {
   return (
     <AppProvider> 
     <CartProvider>
-      <Router />    
+    <PriceProvider>
+      <Router />
+    </PriceProvider>
     </CartProvider>  
     </AppProvider>
   )
