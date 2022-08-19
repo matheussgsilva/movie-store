8e793ce2450f4f94862ae8a42095091c4844f224
Matheussgsilva <matheussgsilva@hotmail.com>
2022-08-18 22:23:51 -0300

    Create movie price in price function

--- src/lib/numbers.js
+++ src/lib/numbers.js
@@ -1,12 +1,12 @@
 export const numbers = {
   price (value) {
-    console.log(value)
-    const moviePrice = value.replace('.', ',')
+    const price = value === 0 ? "9,90" : (value*2.25).toFixed(2)
+    const moviePrice = price.replace('.', ',')
 
     return moviePrice
   },
 
-  card({value}) {
+  card(value) {
     const cardNumber = String(value)
     const cardNumberFormated = `${cardNumber.slice(0,3)} ${cardNumber.slice(4,7)} ${cardNumber.slice(8,11)} ${cardNumber.slice(12,15)}`
 
