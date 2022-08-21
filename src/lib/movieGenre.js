b2af79b06be91b7c6123e93666266ac8e71f14c8
Matheussgsilva <matheussgsilva@hotmail.com>
2022-08-21 20:44:57 -0300

    Create movieGenre function

---
+++ src/lib/movieGenre.js
@@ -0,0 +1,44 @@
+export const movieGenre = (value) => {
+  switch (value) {
+  case 28:
+      return "Ação";
+  case 12:
+      return "Aventura";
+  case 16:
+      return "Animação";
+  case 35:
+      return "Comédia";
+  case 80:
+      return "Crime";
+  case 99:
+      return "Drama";
+  case 18:
+      return "Ação";
+  case 10751:
+      return "Família";
+  case 14:
+      return "Fantasia";
+  case 36:
+      return "História";
+  case 27:
+      return "Terror";
+  case 10402:
+      return "Música";
+  case 9648:
+      return "Mistério";
+  case 10749:
+      return "Romance";
+  case 878:
+      return "Ficção científica";
+  case 10770:
+      return "Cinema TV";
+  case 53:
+      return "Thriller";
+  case 10752:
+      return "Guerra";
+  case 37:
+      return "Faroeste";
+  default:
+      return ""
+  }
+}
